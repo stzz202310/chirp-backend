498a62d28e0c490005b6478a5c5c53ae7973326c
stzz202310 <applest2026@gmail.com>
2026-06-15 18:09:02 -0700

    add local_settings

---
+++ twitter/_local_settings_vagrant.example.py
@@ -0,0 +1,28 @@
+DEBUG = True
+
+# checkout https://www.neilwithdata.com/django-sql-logging
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',   # DEBUG -> INFO
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'INFO',    # DEBUG -> INFO
+            'handlers': ['console'],
+            'propagate': False,  # 不向上冒泡, 避免重复输出
+        },
+    },
+}
+
+HBASE_ENABLED = True
